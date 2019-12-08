efada875ae46c8620fb1459341dd70dbba51cd38
samirankumar11 <samirankumar11@gmail.com>
2019-12-08 15:48:01 +0600

    create coffy shop ui

--- coffee_shop_ui/lib/main.dart
+++ coffee_shop_ui/lib/main.dart
@@ -7,21 +7,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-
 ///========================================================================================
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -29,26 +22,41 @@ class MyHomePage extends StatefulWidget {
 
 ///========================================================================================
 
-
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body:ListView(
-        padding: EdgeInsets.only(left: 15.0),
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.only(left: 15.0),
           children: <Widget>[
             Text(
               "Wellcome, Samiran",
-
+              style: TextStyle(
+                  fontFamily: "varela",
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF473d3A)),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 15.0),
+              child: Container(
+                height: 40.0,
+                width: 40.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                 // image: DecorationImage(image: AssetImage("assets/image/model.png"),
+                  image: DecorationImage(image: AssetImage('images/model.png'),
+                  fit: BoxFit.contain
+                  ),
+                ),
+              ),
             )
-
           ],
+        ),
       ),
-
     );
   }
 }
 
 ///========================================================================================
-
