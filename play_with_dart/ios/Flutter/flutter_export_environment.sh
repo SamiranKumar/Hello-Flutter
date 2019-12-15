01fceaf31982bac20615f8235115cdc32ddc3f8e
samirankumar11 <samirankumar11@gmail.com>
2019-12-15 13:08:56 +0600

    create map app

--- play_with_dart/ios/Flutter/flutter_export_environment.sh
+++ play_with_dart/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/samiran-lt/Program/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/samiran-lt/Development/Devo/androidDevo/android-example-project/appsghor/play_with_dart"
+export "FLUTTER_APPLICATION_PATH=/Users/samiran-lt/Development/Devo/Flutter Projects/Hello-Flutter/play_with_dart"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
